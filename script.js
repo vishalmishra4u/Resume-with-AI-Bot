15fafdbdbd6f2bbc49940c236bb3443759c9077a
vishalmishra4u <mishravishal0912@gmail.com>
2017-03-30 01:44:54 +0530

    changes in styling and icons

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $('.intro-container').click(function() {
         $('.hobbies-container').animate({
          'margin-left': '60%',
-         'margin-top' : '-19.8%'
+         'margin-top' : '-20.2%'
         });
         $('.portfolio-container').animate({
           'margin-left': '80%',
-          'margin-top' : '-19.8%'
+          'margin-top' : '-20.1%'
         });
     });
 });
